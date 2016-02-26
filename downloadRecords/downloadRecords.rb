d86f118b7b0a6bfe05b97bf66ebfaa2904f0fa06
Mehmet Celik <mehmet@celik.be>
2016-02-26 11:30:11 +0100

    added google api key and filtering

--- downloadRecords/downloadRecords.rb
+++ downloadRecords/downloadRecords.rb
@@ -4,11 +4,11 @@ require 'geocoder'
 require 'json'
 require 'logger'
 
-g='AIzaSyC3ntEatI6AaGfzLXamcuRCmC6BzE_R8Is'
 logger = Logger.new(STDOUT)
 Europeana::API.api_key = '8BsYggJzy'
-
+Geocoder.configure(:api_key => 'AIzaSyC3ntEatI6AaGfzLXamcuRCmC6BzE_R8Is')
 e_query = 'foto stadsarchief leuven'
+
 
 
 begin
@@ -18,7 +18,7 @@ begin
   record_count = search['totalResults']
 
   logger.info("Found #{record_count}")
-
+  export_file.puts '['
   while next_cursor = search['nextCursor'] do
     records.each do |record|
       title = record['title'] if(record.has_key?('title'))
@@ -28,18 +28,26 @@ begin
         word =~ /plein|straat|lei|weg|toren|markt|huis|berg|kerk|laan/
       end
 
+      record['lbsLatLon'] = {}
       geo_words.each do |g|
         lat_lon = Geocoder.coordinates("#{g}, leuven, belgium")
         if lat_lon.is_a?(Array)
           logger.info("\t#{g} = #{lat_lon.join(', ')}")
-          record['lbsLatLon'] = lat_lon
+          record['lbsLatLon'] = {g => lat_lon}
         end
       end
+
+      record = record.delete_if {|k,v| ["edmConceptPrefLabelLangAware", "edmDatasetName",
+                               "edmConcept", "edmConceptLabel", "edmPlaceLabel",
+                               "edmPlaceLongitude", "edmPlaceLatitude", "edmPlaceLabelLangAware"].include?(k)}
+
       export_file.puts record.to_json
+      export_file.puts ','
     end
     search = Europeana::API.search(query: e_query, cursor:next_cursor, rows: 100)
     records = search['items']
   end
+  export_file.puts ']'
 
 rescue Exception => e
   logger.error("Search failed for: #{e_query}\n REASON: #{e.message}")
